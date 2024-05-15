efaf0b1a1b30dd4efcc8f7f50db81cf4e22718e4
newmanwang <wcs1011@gmail.com>
2024-05-15 10:32:08 +0800

    加大扫单重新打开时间间隔

--- io-tools/src/main/java/com/ricequant/rqutils/io_tools/tailer/BinaryTailer.java
+++ io-tools/src/main/java/com/ricequant/rqutils/io_tools/tailer/BinaryTailer.java
@@ -195,7 +195,7 @@ public class BinaryTailer {
         }
 
         this.raFile.close();
-        Thread.sleep(1);
+        Thread.sleep(1000);
         this.raFile = new RandomAccessFile(file, "r");
         this.raFile.seek(0);
 
